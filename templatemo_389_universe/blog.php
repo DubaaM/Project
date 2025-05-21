148ee8b94c763ababcf7ac5ae4d85193a003a0bc
DubaaM <duba.marton@gmail.com>
2025-05-21 11:33:33 +0200

    sibebar

--- templatemo_389_universe/blog.php
+++ templatemo_389_universe/blog.php
@@ -4,33 +4,7 @@
     
     
     <div id="templatemo_main_content">
-    	<div id="templatemo_sidebar">
-            <div class="sidebar_content_box">
-            	<h3>Categories</h3>
-                <ul class="sidebar_link">
-                    <li><a href="#">Consectetur adipiscing</a></li>
-                    <li><a href="#">Nullam vulputate est</a></li>
-                    <li><a href="#">Fusce at leo at ipsum</a></li>
-                    <li><a href="#">Donec in risus sed ante</a></li>
-                    <li><a href="#">Vestibulum ante ipsum</a></li>
-                </ul>
-            </div>
-            <div class="sidebar_content_box">
-                <h3>Image Gallery</h3>
-                <ul class="sidebar_gallery">
-                    <li><a href="images/gallery/10.jpg" title="template 1" rel="lightbox[portfolio]"><img src="images/gallery/10_s.jpg" alt="template 1" /></a></li>
-                    <li><a href="images/gallery/11.jpg" title="template 2" rel="lightbox[portfolio]"><img src="images/gallery/11_s.jpg" alt="template 2" /></a></li>
-                    <li><a href="images/gallery/12.jpg" title="template 3" rel="lightbox[portfolio]"><img src="images/gallery/12_s.jpg" alt="template 3" /></a></li>
-                    <li><a href="images/gallery/13.jpg" title="template 4" rel="lightbox[portfolio]"><img src="images/gallery/13_s.jpg" alt="template 4" /></a></li>
-                    <li><a href="images/gallery/05.jpg" title="template 5" rel="lightbox[portfolio]"><img src="images/gallery/05_s.jpg" alt="template 5" /></a></li>
-                    <li><a href="images/gallery/06.jpg" title="template 6" rel="lightbox[portfolio]"><img src="images/gallery/06_s.jpg" alt="template 6" /></a></li>
-                    <li><a href="images/gallery/07.jpg" title="template 7" rel="lightbox[portfolio]"><img src="images/gallery/07_s.jpg" alt="template 7" /></a></li>
-                    <li><a href="images/gallery/08.jpg" title="template 8" rel="lightbox[portfolio]"><img src="images/gallery/08_s.jpg" alt="template 8" /></a></li>
-                    <li><a href="images/gallery/09.jpg" title="template 9" rel="lightbox[portfolio]"><img src="images/gallery/09_s.jpg" alt="template 9" /></a></li>
-                </ul>
-                <div class="clear"></div>
-			</div>
-        </div>
+        <?php include 'partials/sidebar.php'; ?>
         
         <div id="templatemo_content">
         	<div class="post">
